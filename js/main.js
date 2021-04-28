beaa8c59939369a96bdc93521d1d713fdc126a63
YarneDhulstPxl <yarne.dhulst@student.pxl.be>
2021-04-28 23:51:16 +0200

    updated the code and added the needed pictures

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-var hamburger = document.querySelector(".hamburger");
-	var menu = document.querySelector(".menu");
+var slideIndex = 0;
+showSlides();
 
-	hamburger.addEventListener("click", function(){
-		menu.classList.toggle("active");
-	})
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
